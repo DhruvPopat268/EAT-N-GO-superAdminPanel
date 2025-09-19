77720f0fb2d66ea4906e5b6c7fb3fea53c4f17c9
DhruvPopat268 <dhruvpopat268@gmail.com>
2025-09-19 11:17:47 +0530

    updated code for remove utilities

--- frontend/vite/src/menu-items/index.js
+++ frontend/vite/src/menu-items/index.js
@@ -1,6 +1,6 @@
 import dashboard from './dashboard';
 import pages from './pages';
-import utilities from './utilities';
+// import utilities from './utilities';
 import restaurent from './restaurent';
 import payments from './payments';
 import orders from './orders';
@@ -12,7 +12,7 @@ import support from './support';
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, restaurent, orders, payments, customers, rbac, subscription,support, utilities]
+  items: [dashboard, restaurent, orders, payments, customers, rbac, subscription,support]
 };
 
 export default menuItems;
