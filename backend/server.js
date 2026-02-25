b1e885ed286ee762dcd6c18a67f61b247b3699f5
Dhruv popat <dhruvpopat268@gmail.com>
2026-02-25 22:15:53 +0530

    added restaurant onboarding form

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use(cors({
     'http://localhost:3001',
     'http://localhost:3002',
     "https://admin.eatngo.in",
-    "https://restaurant.eatngo.in"
+    "https://restaurant.eatngo.in",
+    "https://eat-n-go-restaurent-registration-fo.vercel.app"
   ],
   credentials: true
 }));
